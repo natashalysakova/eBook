f1eff22ac82337195ad4914d8be8d8b77e7ab77f
natashalysakova <natashalysakova@gmail.com>
2015-04-12 22:21:40 +0300

    Initial commit

---
+++ js/myJs.js
@@ -0,0 +1,70 @@
+/**
+ * Created by hatassska on 12.04.2015.
+ */
+function ShowPage(pageTitle) {
+    $('#body').load(pageTitle + '.html');
+}
+
+function SendResult() {
+
+    ClearResult(false);
+
+    var isCorrect = true;
+    var answers = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1];
+
+    for (var i = 1; i <= 10; i++) {
+        var qName = "question" + i.toString();
+        var val = 0;
+
+        var radios = document.getElementsByName(qName);
+        for (var j = 0, length = radios.length; j < length; j++) {
+            if (radios[j].checked) {
+                // do whatever you want with the checked radio
+                val = radios[j].value;
+                break;
+            }
+        }
+
+        if (val != answers[i - 1]) {
+            isCorrect = false;
+            $('#test' + i).addClass("has-error");
+        }
+        else {
+            $('#test' + i).addClass("has-success");
+        }
+    }
+
+    if (isCorrect) {
+        $('#hurra').show();
+    }
+    else {
+        $('#fail').show();
+    }
+}
+
+function ClearResult(isCleanCheckboxes) {
+
+
+
+    for (var i = 1; i <= 10; i++) {
+        $('#test' + i).removeClass("has-error");
+        $('#test' + i).removeClass("has-success");
+
+        if (isCleanCheckboxes) {
+            var qName = "question" + i.toString();
+            var radios = document.getElementsByName(qName);
+            for (var j = 0, length = radios.length; j < length; j++) {
+                radios[j].checked = false;
+            }
+        }
+    }
+
+    $('#hurra').hide();
+    $('#fail').hide();
+}
+
+$(function () {
+    $('#body').load('main.html');
+});
+
+
